e92132a7221930f160959f50f836fa3e7348d36e
vtuson <vtuson@gmail.com>
2018-02-15 22:48:18 +0100

    Update kubeform_adm.sh
    make node untaint for scheduling

--- kubeform_adm.sh
+++ kubeform_adm.sh
@@ -28,6 +28,8 @@ echo "configuring networking"
 sudo sysctl net.bridge.bridge-nf-call-iptables=1
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
 
 
 
